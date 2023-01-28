933f9be483e72e07cd84e39a0d24c715d3df75a2
Drischdaan <lpboy100@gmail.com>
2023-01-28 17:39:19 +0100

    chore: adds nestjs config package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ConfigService } from '@nestjs/config/dist';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config: ConfigService = app.get<ConfigService>(ConfigService);
+
+  const port: number = config.get<number>('PORT', 3001);
+  await app.listen(port);
 }
 bootstrap();
