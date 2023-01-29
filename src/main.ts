6472465571dabc2d2af32d17dfbca6950ea5385c
Drischdaan <lpboy100@gmail.com>
2023-01-29 14:03:37 +0100

    chore: adds helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config/dist';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config: ConfigService = app.get<ConfigService>(ConfigService);
+
+  app.use(helmet());
 
   app.enableVersioning({
     type: VersioningType.URI,
