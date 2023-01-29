24143b5ada2aa712071c7c3672604f3b35583cc0
Drischdaan <lpboy100@gmail.com>
2023-01-29 13:59:20 +0100

    chore: adds versioning

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config/dist';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -5,6 +6,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config: ConfigService = app.get<ConfigService>(ConfigService);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+    defaultVersion: '1',
+  });
 
   const port: number = config.get<number>('PORT', 3001);
   await app.listen(port);
